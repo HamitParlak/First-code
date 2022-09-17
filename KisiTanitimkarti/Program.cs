33ecaa921b21950052a573c73f83daf98c0d434f
Hamit Parlak <hamitparlak@yahoo.com>
2022-09-18 02:14:02 +0300

    Add files via upload
    1)	Temel Yazdırma Komutları ve Değişkenler

--- KisiTanitimkarti/Program.cs
+++ KisiTanitimkarti/Program.cs
@@ -8,14 +8,100 @@ namespace KisiTanitimkarti
 {
     class Program
     {
+        private static object convert;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Kişi Tanıtım Kartı");
-            Console.WriteLine("Adı: Hamit Parlak");
-            Console.WriteLine("Yaşı: 30");
-            Console.WriteLine("Mesleği: Mühendis");
+            //Console.WriteLine("Kişi Tanıtım Kartı");
+            //Console.WriteLine("Adı: Hamit Parlak");
+            //Console.WriteLine("Yaşı: 30");
+            //Console.WriteLine("Mesleği: Mühendis");
+
+            //değişkenler:
+            //string türü değişkenler üzerinde matematiksel hesap yapmayacağımızdeğişkenler
+            //string değişkenler çift tırnak içerisinde atanır.
+            //string city;
+            //city = "Dublin";
+            //Console.WriteLine(city);
 
 
+            //String capital, country;
+            //capital = "Ankara";
+            //country = "Turkiye";
+            ////Console.Write(capital + " " + country);
+
+
+            //string nameSurname;
+            //nameSurname = "Hamit Parlak";
+            //Console.Write(nameSurname);
+
+            //int değişkenler
+            //sayısal değişkenlerdir. Bunlara aritmetik işlemler uygulayabiliriz.
+
+            //int n1, n2, total;
+            //n1 = 40;
+            //n2 = 85;
+            //total = n1 + n2;
+            //Console.Write(total);
+
+            //int n1,n2,toplama,çıkarma,çarpma,bölme;
+            //n1 = 20;
+            //n2 = 5;
+            //toplama = n1 + n2;
+            //çıkarma = n1 - n2;
+            //çarpma = n1 * n2;
+            //bölme = n1 / n2;
+            //Console.WriteLine("Toplam: " + toplama);
+            //Console.WriteLine("Fark: " + çıkarma);
+            //Console.WriteLine("Çarpım: " + çarpma);
+            //Console.WriteLine("Oran: " + bölme);
+
+            //Console.Write("Adınızı bahşetmez miydiniz: ");
+            //string ad;
+            //ad = Console.ReadLine();
+            //Console.Write("Hoşgeldin " + ad);
+
+            //string ülke, şehir;
+            //Console.Write("Hangi ülkelisin: ");
+            //ülke = Console.ReadLine();
+            //Console.Write("Hangi şehirlisin: ");
+            //şehir = Console.ReadLine();
+            //Console.Write(ülke +" "+ şehir + " havası sert adamı mert bir şehirdir.");
+
+            //program benim konsol ekranına girdiğim tüm değerleri string olarak algılar. 
+            //bu sebeple konsoldan int değişkeni gireceksem şayet değişken dönüşümü yapmam gerekecek.
+
+            //int sayi,kare;
+            //Console.Write("Bir sayı giriniz: ");
+            //sayi = Convert.ToInt32(Console.ReadLine());
+            //kare = sayi * sayi;
+            //Console.WriteLine("Girdiğiniz sayının karesi: " + kare);
+
+            //int kisa,uzun,alan,cevre;
+            //Console.Write("Kısa kenar uzunluğu: ");
+            //kisa = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Uzun kenar uzunluğu: ");
+            //uzun = Convert.ToInt32(Console.ReadLine());
+            //alan = kisa * uzun;
+            //cevre = 2 * (kisa + uzun);
+            //Console.WriteLine("Alan: " + alan + " Çevre: " + cevre);
+
+            //double değişkeni ondalık sayılarda işlem yapabilmemizi sağlayan değişken türüdür.
+            //ondalık kısım noktayla ayrılır virgülle değil!
+
+            //double not1, not2, not3, ortalama;
+            //Console.Write("1. sınav sonucunu giriniz: ");
+            //not1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("2. sınav sonucunu giriniz: ");
+            //not2 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("3. sınav notunu giriniz: ");
+            //not3 = Convert.ToDouble(Console.ReadLine());
+            //ortalama = (not1 + not2 + not3) / 3;
+            //Console.Write("Not ortalaması: " + ortalama);
+
+
+
+         
             Console.ReadLine();
 
         }
