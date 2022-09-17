0bf8237a91a092f4230e928ef2b0d93ea46874a5
Mutfak Robotu <Mutfak Robotu@HAMIT>
2022-09-18 00:19:44 +0300

    Add project files.

---
+++ KisiTanitimkarti/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KisiTanitimkarti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kişi Tanıtım Kartı");
+            Console.WriteLine("Adı: Hamit Parlak");
+            Console.WriteLine("Yaşı: 30");
+            Console.WriteLine("Mesleği: Mühendis");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
